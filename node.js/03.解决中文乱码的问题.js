4481d633bf6547cd65705ff167af8ecc3d9e47c5
hao_go <761999100@qq.com>
2021-05-20 11:51:34 +0800

    解决中文乱码问题，报错了，原因不明

--- node.js/03.解决中文乱码的问题.js
+++ node.js/03.解决中文乱码的问题.js
@@ -10,9 +10,9 @@ const http = require('http')
 const server =http.createServer()
 server.on('request',(req,res)=>{
     //定义一个字符串，包含中文的内容
-    const str = '您请求的URL 地址是${req.url},请求的method类型为${req.method}'
+    const str = '您请求的URL 地址是 ${req.url},请求的method类型为${req.method}'
     //调用 res.setHeader()方法，设置Content-Type响应头，解决中文乱码问题
-    res.setHeader('Content-Type ','text/html;charset=utf-8')
+    res.setHeader('Content-Type ','text/html; charset=utf-8')//没执行成功，不知道什么原因
     //res.end()将内容响应给客户端
     res.end(str)
 })
