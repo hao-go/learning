7025f33277be3595eff6a44d4064f3ed076d98ed
hao_go <761999100@qq.com>
2021-05-25 10:52:52 +0800

    配置打包入口和出口

--- webpack/webpack_study/src/index.js
+++ webpack/webpack_study/src/index.js
@@ -1,6 +1,6 @@
 import $ from 'jquery'
 //这句属于Es6的模块化语法，浏览器对其兼容性并不是很好，所以html里引用这个文件不起作用，故需要webpack将其打包，让html文件引用打包后的文件main.js
 $(function(){
-    $('li:odd').css('backgroundColor','pink')
+    $('li:odd').css('backgroundColor','blue')
     $('li:even').css('backgroundColor','lightblue')
 })
--- webpack/webpack_study/webpack.config.js
+++ webpack/webpack_study/webpack.config.js
@@ -1,6 +1,19 @@
+const path = require("path");
+
+
 module.exports={//通过这种方式向外暴露一个配置项
     
     //编译模式
     mode:"development",//可以设置为development(开发模式)，production(发布模式),但是这个没起作用
+    //设置入口文件路径
+    entry: path.join(__dirname,"./src/index.js"),
+    //设置出口文件
+    output:{
+        //设置路径
+        path:path.join(__dirname,"./dist"),
+        //设置文件名
+        filename:"bundle.js"
+    }
+
 
 };
