35eef5bfa010210aba3858ef89b846ff645a7efe
hao_go <761999100@qq.com>
2021-05-25 21:14:58 +0800

    单组件初步了解，初步配置

---
+++ webpack/webpack_study/src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>这是app根组件</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+    data(){
+        return{};
+    },
+    methods:{}
+}
+</script>
+
+
+<style scoped>
+/* scoped表示该组件私有这个样式 */
+h1{
+    color: red;
+}
+</style>
